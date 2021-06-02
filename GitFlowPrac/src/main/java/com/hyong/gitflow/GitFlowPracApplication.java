4f54a25cc0923fd04e2ee719e720abc9496296e5
DESKTOP-4RR8UEB\siggr <siggraph.k@gmail.com>
2021-06-03 01:53:59 +0900

    기초생성파일

---
+++ GitFlowPrac/src/main/java/com/hyong/gitflow/GitFlowPracApplication.java
@@ -0,0 +1,13 @@
+package com.hyong.gitflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitFlowPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitFlowPracApplication.class, args);
+	}
+
+}
