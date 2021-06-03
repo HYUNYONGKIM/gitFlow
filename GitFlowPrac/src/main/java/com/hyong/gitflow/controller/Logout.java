15c61e2d66360a85123b2d32fa3089a9821ccf08
DESKTOP-4RR8UEB\siggr <siggraph.k@gmail.com>
2021-06-03 13:06:34 +0900

    머지용

--- GitFlowPrac/src/main/java/com/hyong/gitflow/controller/Logout.java
+++ GitFlowPrac/src/main/java/com/hyong/gitflow/controller/Logout.java
@@ -5,5 +5,7 @@ public class Logout {
 		// TODO Auto-generated method stub
 		System.out.println("지금은 로그아웃 기능을 개발 중입니다.");
 		System.out.println("한번 수정하였습니다.");
+		System.out.println("이것이 보이면 merge가 잘 된것이빈다.");
+
 	}
 }
