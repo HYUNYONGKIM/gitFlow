dc3d1f8f243a18eeaf3f8219caa1725f230692ef
DESKTOP-4RR8UEB\siggr <siggraph.k@gmail.com>
2021-06-03 12:46:46 +0900

    핫픽스 오류 수정

--- GitFlowPrac/src/main/java/com/hyong/gitflow/controller/OtherController.java
+++ GitFlowPrac/src/main/java/com/hyong/gitflow/controller/OtherController.java
@@ -3,6 +3,7 @@ package com.hyong.gitflow.controller;
 public class OtherController {
 	private void print() {
 		// TODO Auto-generated method stub
-		이 표시가 보이면 merge가 된것입니다.
+		//이 표시가 보이면 merge가 된것입니다.
+		System.out.println("핫픽스 오류 수정하였습니다.");
 	}
 }
