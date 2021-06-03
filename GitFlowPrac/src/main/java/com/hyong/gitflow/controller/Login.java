4465044aea009e83cdfb6e0798dfb4e1890d57d5
DESKTOP-4RR8UEB\siggr <siggraph.k@gmail.com>
2021-06-03 13:06:12 +0900

    머지용

--- GitFlowPrac/src/main/java/com/hyong/gitflow/controller/Login.java
+++ GitFlowPrac/src/main/java/com/hyong/gitflow/controller/Login.java
@@ -5,5 +5,6 @@ public class Login {
 		// TODO Auto-generated method stub
 		System.out.println("지금은 로그인기능을 개발 중입니다.");
 		System.out.println("첫번째수정");
+		System.out.println("이것이 보이면 merge가 잘 된것이빈다.");
 	}
 }
