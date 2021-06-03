09b15b221a3475959642c021e102c24084c862a7
DESKTOP-4RR8UEB\siggr <siggraph.k@gmail.com>
2021-06-03 12:42:43 +0900

    로그아웃 개발중

--- GitFlowPrac/src/main/java/com/hyong/gitflow/controller/Logout.java
+++ GitFlowPrac/src/main/java/com/hyong/gitflow/controller/Logout.java
@@ -1,5 +1,8 @@
 package com.hyong.gitflow.controller;
 
 public class Logout {
-
+	private void print() {
+		// TODO Auto-generated method stub
+		System.out.println("지금은 로그아웃 기능을 개발 중입니다.");
+	}
 }
